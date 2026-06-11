c977862f6f8e77978ed1a8d2456c71ae1aa0efba
Antony Amal Rekshin <antonyamalrekshin@gmail.com>
2026-06-11 15:13:48 +0530

    Update OAuth2LoginSuccessHandler.java

--- TimeTable-Backend/src/main/java/com/example/timetable/security/OAuth2LoginSuccessHandler.java
+++ TimeTable-Backend/src/main/java/com/example/timetable/security/OAuth2LoginSuccessHandler.java
@@ -51,7 +51,7 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
         String token = jwtUtil.generateToken(user);
         
         // Redirect to frontend React app callback URL
-        String redirectUrl = "http://localhost:5173/timeTable_management/auth/callback?token=" + token;
+        String redirectUrl = "https://sprightly-taffy-950990.netlify.app/auth/callback?token=" + token;
         getRedirectStrategy().sendRedirect(request, response, redirectUrl);
     }
 }
