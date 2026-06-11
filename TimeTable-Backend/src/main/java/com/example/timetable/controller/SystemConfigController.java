94fc82d8a41f79f5514dc56d0d8db8b308c18686
Antony Amal Rekshin <antonyamalrekshin@gmail.com>
2026-06-11 15:51:41 +0530

    Update SystemConfigController.java

--- TimeTable-Backend/src/main/java/com/example/timetable/controller/SystemConfigController.java
+++ TimeTable-Backend/src/main/java/com/example/timetable/controller/SystemConfigController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/config")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://classtimescheduler.netlify.app")
 public class SystemConfigController {
 
     private final SystemConfigRepository repository;
