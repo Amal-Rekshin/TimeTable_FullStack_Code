0fdbbe8113953d1893ed0061e961387522a910d5
Antony Amal Rekshin <antonyamalrekshin@gmail.com>
2026-06-11 15:52:08 +0530

    Update CORS origin for UserController

--- TimeTable-Backend/src/main/java/com/example/timetable/controller/UserController.java
+++ TimeTable-Backend/src/main/java/com/example/timetable/controller/UserController.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://classtimescheduler.netlify.app")
 public class UserController {
 
     @Autowired
