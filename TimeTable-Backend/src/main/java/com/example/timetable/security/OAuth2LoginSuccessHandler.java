45a47100538af34fc50859275fc32de192a3cdf6
Antony Amal Rekshin <antonyamalrekshin@gmail.com>
2026-06-11 16:52:36 +0530

    Update OAuth2LoginSuccessHandler.java

--- TimeTable-Backend/src/main/java/com/example/timetable/security/OAuth2LoginSuccessHandler.java
+++ TimeTable-Backend/src/main/java/com/example/timetable/security/OAuth2LoginSuccessHandler.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.IOException;
 import java.util.Optional;
